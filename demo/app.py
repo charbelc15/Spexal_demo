b6fe0bad2173776a19623a276ff300573867dd23
Charbel <charbelc15@gmail.com>
2022-04-20 12:35:45 +0300

    Setting up mysql config DB + passing its data to index.html

--- demo/app.py
+++ demo/app.py
@@ -4,10 +4,26 @@ from flaskext.mysql import MySQL
 import pymysql
  
 app = Flask(__name__)
-  
+
+mysql = MySQL()
+   
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'testingdb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+ 
+    cur.execute('SELECT * FROM employee')
+    data = cur.fetchall()
+  
+    cur.close()
+    return render_template('index.html', employee = data)
 
 #starting the app 
 if __name__ == "__main__":
