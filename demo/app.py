ed88da9916429d1142067acfeb51a8e207349139
Charbel <charbelc15@gmail.com>
2022-04-22 16:04:46 +0300

    adding employee to DB via POST form + flashed message on success

--- demo/app.py
+++ demo/app.py
@@ -4,8 +4,10 @@ from flaskext.mysql import MySQL
 import pymysql
  
 app = Flask(__name__)
+app.secret_key = 'Charbel123'
 
 mysql = MySQL()
+
    
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -19,12 +21,27 @@ def Index():
     conn = mysql.connect()
     cur = conn.cursor(pymysql.cursors.DictCursor)
  
-    cur.execute('SELECT * FROM employee')
+    cur.execute('SELECT * FROM employee')   #fetching all info from DB in cursor 
     data = cur.fetchall()
   
     cur.close()
-    return render_template('index.html', employee = data)
+    return render_template('index.html', employee = data) # sending them to index.hml
 
+@app.route('/add_employee', methods=['POST']) #connected to the url form in index.html 
+def add_employee():
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    if request.method == 'POST':            #retrieving variables from from + sending them to DB
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        Address = request.form['Address']
+        Department = request.form['Department']
+        cur.execute("INSERT INTO employee (name, email, phone, Address, Department) VALUES (%s,%s,%s,%s,%s)", (fullname, email, phone,Address,Department))
+        conn.commit()
+        flash('Employee Added successfully')
+        return redirect(url_for('Index'))
+ 
 #starting the app 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
